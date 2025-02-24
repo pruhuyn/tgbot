535299b246be3dd183b48c86b36c3dfe8ef665b0
pruhuyn <otchimkircicha@gmail.com>
2025-02-24 17:08:55 +0200

    asdghrt

--- bot.py
+++ bot.py
@@ -1,31 +1,33 @@
 import sqlite3
 from aiogram import Bot, types
-from aiogram.fsm.context import FSMContext
+from aiogram.client import Application
 from aiogram.fsm.state import State, StatesGroup
-from aiogram import Dispatcher
-from aiogram import Application  # Это правильный импорт для 3.x версии
+from aiogram.fsm.context import FSMContext
+from aiogram.filters import Command
 
 API_TOKEN = '8116355818:AAF3yBKLv4XpZn_-YqE7A5EeKVy905dNa0M'
 
-# Инициализация бота и приложения
+# Создаем бота и приложение
 bot = Bot(token=API_TOKEN)
 app = Application.builder().token(API_TOKEN).build()
 
-# Определяем состояния
+# Описание состояний
 class Form(StatesGroup):
     username = State()
 
-@app.message_handler(commands=["login"])
+# Команда для начала авторизации
+@app.message_handler(Command("login"))
 async def cmd_login(message: types.Message, state: FSMContext):
     await message.answer("Введите ваш username:")
     await Form.username.set()
 
+# Обработка введенного username
 @app.message_handler(state=Form.username)
 async def check_username(message: types.Message, state: FSMContext):
     username_to_check = message.text
 
     # Подключение к базе данных
-    conn = sqlite3.connect(r'C:\Users\Alexei\Desktop\tgbot\instance.users.db')  # Путь к базе данных
+    conn = sqlite3.connect('C:\Users\Alexei\Desktop\tgbot\instance.users.db')  # Укажи путь к своей базе данных
     cursor = conn.cursor()
 
     cursor.execute("SELECT username FROM user WHERE username=?", (username_to_check,))
@@ -37,7 +39,7 @@ async def check_username(message: types.Message, state: FSMContext):
         await message.answer("Пользователь не найден. Попробуйте снова.")
 
     conn.close()
-    await state.finish()  # Заканчиваем состояние
+    await state.finish()
 
 if __name__ == '__main__':
     app.run_polling()
